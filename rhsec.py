6b44df77fffeb313d2426d52453553fec2a2fe8a
ChrisWilkes33 <chriswilkes33@gmail.com>
2019-08-04 13:03:09 -0500

    added time/product limiters
    added a time limiter for cvrf requests, added product limiter for cve requests

--- rhsec.py
+++ rhsec.py
@@ -1,16 +1,19 @@
 import json
 import sys
 import requests
+from datetime import datetime, timedelta
+
 
 url = "https://access.redhat.com/labs/securitydataapi"
 
 
 def get_cve_json(baseurl):
     """
-    takes RedHat security data api url and returns a json of every CVE
+    takes RedHat security data api url and product variable and returns a json of every CVE for that product
     """
     endpoint = "/cve.json"
-    r = requests.get(baseurl+endpoint)
+    params = 'product=linux 8'
+    r = requests.get(baseurl+endpoint+'?'+params)
     #pretty_r = json.loads(r.text)
     #return json.dumps(pretty_r, indent=2)
     return r.json()
@@ -18,17 +21,20 @@ def get_cve_json(baseurl):
 
 def get_cvrf_json(baseurl):
     """
-    takes RedHat security data api url and returns a json block of every CVRF entry
+    takes RedHat security data api url and returns a json block of every CVRF entry last 30 days
     """
     endpoint = "/cvrf.json"
-    r = requests.get(baseurl + endpoint)
+    date = datetime.now() - timedelta(days=30)
+    params = 'after='+str(date.date())
+    r = requests.get(baseurl + endpoint + '?' + params)
     #pretty_r = json.loads(r.text)
     #return json.dumps(pretty_r, indent=2)
     return r.json()
 
 
-stuff = get_cvrf_json(url)
-#print(stuff)
-for cvrf in stuff:
+#stuff = get_cvrf_json(url)
+#for cvrf in stuff:
     print(cvrf['RHSA'], cvrf['severity'], cvrf['released_on'], cvrf['CVEs'], cvrf['released_packages'])
 
+stuff = get_cve_json(url)
+print(stuff)
